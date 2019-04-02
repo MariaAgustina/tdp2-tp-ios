9f8c07e0c39d901b5de332a67907d6e1172cb36e
agustina markosich <agustina.markosich@mercadolibre.com>
2019-04-02 15:56:37 -0300

    *search trip with origin and destiny added *changed api key

--- PetTransport/PetTransport/Controllers/BookTransportViewController.m
+++ PetTransport/PetTransport/Controllers/BookTransportViewController.m
@@ -8,11 +8,19 @@
 
 #import "BookTransportViewController.h"
 #import <GoogleMaps/GoogleMaps.h>
+#import <GooglePlaces/GooglePlaces.h>
 #import "LocationManager.h"
+#import "Trip.h"
 
-@interface BookTransportViewController () <LocationManagerDelegate>
+
+@interface BookTransportViewController () <LocationManagerDelegate, GMSAutocompleteViewControllerDelegate>
+
 
 @property (weak, nonatomic) IBOutlet GMSMapView *mapView;
+@property (strong, nonatomic) GMSAutocompleteFilter *filter;
+
+@property (strong,nonatomic) Trip* trip;
+@property (nonatomic, copy) void (^autocompleteCompletionBlock)(GMSPlace *);
 
 @end
 
@@ -26,6 +34,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"Estoy en Book Transport");
+    self.trip = [Trip new];
 }
 
 -(void)viewWillAppear:(BOOL)animated {
@@ -47,6 +56,51 @@
     return marker;
 }
 
+// Present the autocomplete view controller when the button is pressed.
+
+- (void)autocompleteClickedCompletionBlock:(void (^)(GMSPlace *))completionBlock{
+    self.autocompleteCompletionBlock = completionBlock;
+    
+    GMSAutocompleteViewController *acController = [[GMSAutocompleteViewController alloc] init];
+    acController.delegate = self;
+    
+    // Specify the place data types to return.
+    GMSPlaceField fields = (GMSPlaceFieldName | GMSPlaceFieldPlaceID);
+    acController.placeFields = fields;
+    
+    // Specify a filter.
+    self.filter = [[GMSAutocompleteFilter alloc] init];
+    self.filter.type = kGMSPlacesAutocompleteTypeFilterAddress;
+    acController.autocompleteFilter = self.filter;
+    
+    // Display the autocomplete view controller
+    [self presentViewController:acController animated:YES completion:nil];
+}
+
+
+- (IBAction)originButtonPressed:(id)sender {
+    [self autocompleteClickedCompletionBlock: ^(GMSPlace *place) {
+        self.trip.origin = place;
+    }];
+}
+
+- (IBAction)destinyButtonPressed:(id)sender {
+    [self autocompleteClickedCompletionBlock: ^(GMSPlace *place) {
+        self.trip.destiny = place;
+    }];
+}
+
+- (IBAction)searchTripButtonPressed:(id)sender {
+    NSLog(@"Origen: %@",self.trip.origin.name);
+    NSLog(@"Destino: %@",self.trip.destiny.name);
+    
+    if(![self.trip isValid]){
+        //TODO: mensaje de que elija origen y destino
+        return;
+    }
+    //TODO: crear viaje
+}
+
 #pragma mark - LocationManagerDelegate
 - (void)didFetchCurrentLocation: (struct LocationCoordinate)coordinate {
     GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:coordinate.latitude
@@ -59,6 +113,39 @@
 
 - (void)didFailFetchingCurrentLocation {
     NSLog(@"no pudo traer la location");
+}
+
+
+#pragma mark - GMSAutocompleteViewControllerDelegate
+
+// Handle the user's selection.
+- (void)viewController:(GMSAutocompleteViewController *)viewController
+didAutocompleteWithPlace:(GMSPlace *)place {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    // Do something with the selected place.
+    NSLog(@"Place name %@", place.name);
+    NSLog(@"Place ID %@", place.placeID);
+    NSLog(@"Place attributions %@", place.attributions.string);
+    self.autocompleteCompletionBlock(place);
+}
+
+- (void)viewController:(GMSAutocompleteViewController *)viewController
+didFailAutocompleteWithError:(NSError *)error {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    // TODO: handle the error.
+    NSLog(@"Error: %@", [error description]);
+}
+
+- (void)wasCancelled:(GMSAutocompleteViewController *)viewController {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)didRequestAutocompletePredictions:(GMSAutocompleteViewController *)viewController {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+- (void)didUpdateAutocompletePredictions:(GMSAutocompleteViewController *)viewController {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
 }
 
 @end
---
+++ PetTransport/PetTransport/Trip/Trip.h
@@ -0,0 +1,24 @@
+//
+//  Trip.h
+//  PetTransport
+//
+//  Created by agustina markosich on 4/2/19.
+//  Copyright © 2019 agustina markosich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <GooglePlaces/GooglePlaces.h>
+#import <GoogleMaps/GoogleMaps.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Trip : NSObject
+
+@property (strong, nonatomic) GMSPlace *origin;
+@property (strong, nonatomic) GMSPlace *destiny;
+
+- (BOOL)isValid;
+
+@end
+
+NS_ASSUME_NONNULL_END
