79fec403a6cd77af62174c8ff478c7e3449577cb
Kaoru Heanna <kaoru.heanna@trocafone.com>
2019-04-14 15:58:40 -0300

    start registration form

---
+++ PetTransport/PetTransport/Controllers/FacebookViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterClientViewController.h
+//  PetTransport
+//
+//  Created by Kaoru Heanna on 4/13/19.
+//  Copyright © 2019 agustina markosich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacebookViewController : UIViewController
+
+@end
--- PetTransport/PetTransport/Controllers/RegisterClientViewController.m
+++ PetTransport/PetTransport/Controllers/RegisterClientViewController.m
@@ -2,20 +2,20 @@
 //  RegisterClientViewController.m
 //  PetTransport
 //
-//  Created by Kaoru Heanna on 4/13/19.
+//  Created by Kaoru Heanna on 4/14/19.
 //  Copyright © 2019 agustina markosich. All rights reserved.
 //
 
 #import "RegisterClientViewController.h"
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <FBSDKLoginKit/FBSDKLoginKit.h>
-#import "FbProfileManager.h"
 
-@interface RegisterClientViewController () <FBSDKLoginButtonDelegate, FbProfileManagerDelegate>
+@interface RegisterClientViewController ()
 
-@property (weak, nonatomic) IBOutlet UIButton *customFbButton;
-@property (weak, nonatomic) IBOutlet FBSDKLoginButton *fbButton;
-@property (strong, nonatomic) FbProfileManager *fbProfileManager;
+@property (weak, nonatomic) IBOutlet UITextField *firstNameField;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameField;
+@property (weak, nonatomic) IBOutlet UITextField *addressField;
+@property (weak, nonatomic) IBOutlet UITextField *phoneField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UIDatePicker *birthdatePicker;
 
 @end
 
@@ -24,65 +24,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.title = @"Registrarme como cliente";
+    self.title = @"Registro";
     
-    self.fbProfileManager = [[FbProfileManager alloc] initWithDelegate:self];
+    NSLog(@"name: %@!", self.profile.firstName);
+    NSLog(@"lastName: %@!", self.profile.lastName);
+    NSLog(@"userId: %@!", self.profile.fbUserId);
+    self.firstNameField.text = self.profile.firstName;
+    self.lastNameField.text = self.profile.lastName;
     
-    self.fbButton.delegate = self;
-    [self.fbButton setAttributedTitle:[[NSAttributedString alloc] initWithString:@"logueate fiera"]
-                                forState:UIControlStateNormal];
-    
-    [self loadProfile];
-}
-
-- (void)loadProfile {
-    [self.fbProfileManager loadProfile];
-}
-
-- (IBAction)customFbButtonPressed:(id)sender {
-    NSLog(@"CUSTOM BUTTON PRESSED");
-    FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
-    [login
-     logInWithReadPermissions: @[@"public_profile"]
-     fromViewController:self
-     handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
-         if (error) {
-             NSLog(@"Process error");
-         } else if (result.isCancelled) {
-             NSLog(@"Cancelled");
-         } else {
-             NSLog(@"Logged in");
-             [self loadProfile];
-         }
-     }];
-}
-
-# pragma mark - FBSDKLoginButtonDelegate methods
-- (void)  loginButton:(FBSDKLoginButton *)loginButton
-didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result
-                error:(NSError *)error{
-    NSLog(@"Login Completed");
-    [self loadProfile];
-}
-- (void) loginButtonDidLogOut:(FBSDKLoginButton *)loginButton{
-    NSLog(@"Logout");
-}
-
-#pragma mark - FbProfileManagerDelegate methods
-- (void)didLoadProfile: (FBSDKProfile *)profile {
-    NSLog(@"did load profile");
-    NSLog(@"name: %@!", profile.firstName);
-    NSLog(@"lastName: %@!", profile.lastName);
-    NSLog(@"name: %@!", profile.name);
-    NSLog(@"userId: %@!", profile.userID);
-}
-
-- (void)notLoggedInFb {
-    NSLog(@"no estoy logueado en FB");
-}
-
-- (void)didFailedLoadingProfile: (NSError *)error {
-    NSLog(@"no pude recuperar el profile");
 }
 
 
