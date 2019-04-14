9c88dac7a406d81c67eb417fb90df4c7729b292a
Kaoru Heanna <kaoru.heanna@trocafone.com>
2019-04-14 16:30:02 -0300

    styling form

--- PetTransport/PetTransport/Controllers/RegisterClientViewController.m
+++ PetTransport/PetTransport/Controllers/RegisterClientViewController.m
@@ -7,15 +7,18 @@
 //
 
 #import "RegisterClientViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface RegisterClientViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *formWrapper;
 @property (weak, nonatomic) IBOutlet UITextField *firstNameField;
 @property (weak, nonatomic) IBOutlet UITextField *lastNameField;
 @property (weak, nonatomic) IBOutlet UITextField *addressField;
 @property (weak, nonatomic) IBOutlet UITextField *phoneField;
 @property (weak, nonatomic) IBOutlet UITextField *emailField;
 @property (weak, nonatomic) IBOutlet UIDatePicker *birthdatePicker;
+@property (weak, nonatomic) IBOutlet UIButton *registrationButton;
 
 @end
 
@@ -26,12 +29,21 @@
     
     self.title = @"Registro";
     
-    NSLog(@"name: %@!", self.profile.firstName);
-    NSLog(@"lastName: %@!", self.profile.lastName);
-    NSLog(@"userId: %@!", self.profile.fbUserId);
+    self.formWrapper.layer.borderColor = [UIColor blackColor].CGColor;
+    self.formWrapper.layer.borderWidth = 1.0f;
     self.firstNameField.text = self.profile.firstName;
+    [self.firstNameField setEnabled:NO];
     self.lastNameField.text = self.profile.lastName;
-    
+    [self.lastNameField setEnabled:NO];
+    [self validateFields];
+}
+
+- (IBAction)registerButtonPressed:(id)sender {
+    NSLog(@"Registrarme");
+}
+
+- (void)validateFields {
+    [self.registrationButton setEnabled:NO];
 }
 
 
