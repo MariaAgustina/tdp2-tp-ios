7e132c310b71472542d59376d74b6a06d9d35e42
Kaoru Heanna <kaoru.heanna@trocafone.com>
2019-04-18 14:46:41 -0300

    validate email

--- PetTransport/PetTransport/Controllers/RegisterClientViewController.m
+++ PetTransport/PetTransport/Controllers/RegisterClientViewController.m
@@ -91,8 +91,13 @@
     if (![self isValidText:self.emailField.text]){
         return NO;
     }
-    // TODO: agregar mask
-    return YES;
+    return [self isValidEmailWithString:self.emailField.text];
+}
+
+- (BOOL)isValidEmailWithString:(NSString*)email {
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:email];
 }
 
 - (BOOL)isValidBirthdate {
