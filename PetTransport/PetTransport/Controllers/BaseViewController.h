cc1768d1a38730d8a26055e5c0817a5b6d364f16
agustina markosich <agustina.markosich@mercadolibre.com>
2019-05-05 19:23:08 -0300

    add loading

---
+++ PetTransport/PetTransport/Controllers/BaseViewController.h
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.h
+//  PetTransport
+//
+//  Created by agustina markosich on 5/5/19.
+//  Copyright © 2019 agustina markosich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BaseViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
