db147581b1945eb678fdcd9669bb6f7a8a7a17eb
Kaoru Heanna <kaoru.heanna@trocafone.com>
2019-05-05 16:22:20 -0300

    staging env api url

--- PetTransport/PetTransport/constants.m
+++ PetTransport/PetTransport/constants.m
@@ -9,11 +9,17 @@
 #import <Foundation/Foundation.h>
 #import "constants.h"
 
-#define USE_LOCALHOST 1
+#define LOCALHOST 0
+#define STAGING 1
+#define PROD 2
 
-#if USE_LOCALHOST == 1
+#define ENV LOCALHOST
+
+
+#if ENV == LOCALHOST
 NSString* const API_BASE_URL = @"http://localhost:3000";
+#elif ENV == STAGING
+NSString* const API_BASE_URL = @"http://stagingserver7547.herokuapp.com";
 #else
 NSString* const API_BASE_URL = @"http://server7547.herokuapp.com";
 #endif
-
