f6bfe2ec50efb80f21e07d8502723ddc3845398a
Kaoru Heanna <kaoru.heanna@trocafone.com>
2019-05-30 20:06:03 -0300

    month titles

--- PetTransport/PetTransport/Controllers/Driver/SummaryViewController.m
+++ PetTransport/PetTransport/Controllers/Driver/SummaryViewController.m
@@ -24,9 +24,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.navigationItem setHidesBackButton:YES];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self updateTitles];
+}
+
+- (void)updateTitles {
+    NSDateFormatter *monthFormatter=[[NSDateFormatter alloc] init];
+    [monthFormatter setDateFormat:@"MM"];
+    
+    NSDateFormatter *yearFormatter=[[NSDateFormatter alloc] init];
+    [yearFormatter setDateFormat:@"yyyy"];
+    
+    NSDate *currentDate = [NSDate date];
+    NSString *currentMonthNumber = [monthFormatter stringFromDate:currentDate];
+    NSString *currentMonthLabel = [self getMonthLabel:currentMonthNumber];
+    NSString *currentYearLabel = [yearFormatter stringFromDate:currentDate];
+    self.currentMonthTitle.text = [NSString stringWithFormat:@"%@ - %@", currentMonthLabel, currentYearLabel];
+    
+    NSDate *previousMonthDate = [self getPreviousMonth];
+    NSString *previousMonthNumber = [monthFormatter stringFromDate:previousMonthDate];
+    NSString *previousMonthLabel = [self getMonthLabel:previousMonthNumber];
+    NSString *previousYearLabel = [yearFormatter stringFromDate:previousMonthDate];
+    self.previousMonthTitle.text = [NSString stringWithFormat:@"%@ - %@", previousMonthLabel, previousYearLabel];
+}
+
+- (NSDate*)getPreviousMonth {
+    NSDate *today = [[NSDate alloc] init];
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *offsetComponents = [[NSDateComponents alloc] init];
+    [offsetComponents setMonth:-1];
+    NSDate *lastMonth = [gregorian dateByAddingComponents:offsetComponents toDate:today options:0];
+    return lastMonth;
+}
+
+- (NSString*)getMonthLabel: (NSString*)monthNumber {
+    NSDictionary *months = @{
+                             @"01": @"Enero",
+                             @"02": @"Febrero",
+                             @"03": @"Marzo",
+                             @"04": @"Abril",
+                             @"05": @"Mayo",
+                             @"06": @"Junio",
+                             @"07": @"Julio",
+                             @"08": @"Agosto",
+                             @"09": @"Septiembre",
+                             @"10": @"Octubre",
+                             @"11": @"Noviembre",
+                             @"12": @"Diciembre",
+                             };
+    return [months objectForKey:monthNumber];
 }
 
 - (IBAction)buttonPressed:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 @end
